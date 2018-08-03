ff4cdfbf8a2b3672a8de998fbe35aab1b5db5427
Robert Link <robert.link@pnnl.gov>
2018-08-03 09:04:18 -0400

    Add python package skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dnnclim',
+      version='0.0.1-1000',
+      description='Deep Neural Network climate scenarios',
+      url='http://github.com/JGCRI/dnnclim',
+      author='Robert Link',
+      author_email='robert.link@pnnl.gov',
+      license='GPL-2',
+      packages=['dnnclim'],
+      zip_safe=True)
