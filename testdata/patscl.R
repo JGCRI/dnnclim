ccaf3c8a2599e0e66687f10b9460123a572e813b
Robert Link <robert.link@pnnl.gov>
2018-10-11 09:56:20 -0400

    Compute RMSE as well as MAE for pattern scaling.

--- testdata/patscl.R
+++ testdata/patscl.R
@@ -1,6 +1,7 @@
 #### Run a linear pattern scaling calculation on a set of input files.
-### Running this code on the full dataset used to train dnnclim results in 
+### Running this code on the full dataset used to train dnnclim results in
 ### dev set MAE of (2.91 K, 1.21 mm/day)
+###        RMSE of (4.33 K, 2.05 mm/day)
 
 library('assertthat')
 
@@ -10,43 +11,47 @@ run_ps <- function(filelist, varname)
     ## the name of a grid file.  All of the files should be of the same type
     ## (i.e., temperature or precip) The name of the global mean file will be
     ## formed by appending '_gavg.txt' to the name of the grid file.
-    
+
     fld <- do.call(rbind, lapply(filelist, function(fn) {readncfield(fn,varname)}))
     globmean <- do.call(rbind, lapply(filelist, readglobmean))
-    
+
     assert_that(nrow(fld) == nrow(globmean))
-    
+
     ## Separate the data we read into train and test sets
     n <- nrow(fld)
     ntrain = as.integer(round(0.75*n))
     message('ntrain = ', ntrain)
     message('ntest = ', n-ntrain)
     message('grid size = ', ncol(fld))
-    
+
     permvec <- sample(1:n, n)
     trainidx <- sort(permvec[1:ntrain])
     testidx <- sort(permvec[(ntrain+1):n])
-    
+
     ## Fit the linear pattern scaling to the training data
     trainfld <- fld[trainidx,]
     traingmean <- globmean[trainidx,]
     pscl <- fldgen::pscl_analyze(trainfld, traingmean)
-    
+
     ## Apply the pattern scaling coefficients to the test data
     testfld <- fld[testidx,]
     testgmean <- globmean[testidx,]
     psout <- fldgen::pscl_apply(pscl, testgmean)
-    
-    ## return the mean absolute error
-    mean(abs(psout-testfld))
+
+    ## return the mean absolute error and rmserror
+    diff <- psout-testfld
+    mae <- mean(abs(diff))
+    rmse <- sqrt(mean(diff*diff))
+
+    c(mae=mae, rmse=rmse)
 }
 
 readncfield <- function(filename, varname,
-                        latvar='lat', lonvar='lon', timevar='time') 
+                        latvar='lat', lonvar='lon', timevar='time')
 {
     ## This is mostly cribbed from fldgen::read.temperatures
     tann <- ncdf4::nc_open(filename)
-    
+
     ## fld3d should have dimensions of time x lat x lon in the netcdf file.
     ## Because R uses Fortran array layout, this gets reversed to lon x lat x time.
     fld3d <- ncdf4::ncvar_get(tann, var=varname)
@@ -58,16 +63,16 @@ readncfield <- function(filename, varname,
     ntime <- length(time)
     timeout <- seq_along(time) - 1
     ncdf4::nc_close(tann)
-    
+
     assert_that(all(dim(fld3d) == c(nlon, nlat, ntime)))
 
     ## reorganize and flatten the 3-D array into a 2d array of ntime x ngrid
     ## As we do this, we will also make latitude the most rapidly varying index
     ## for the individual time slices.
-    
+
     fld <- aperm(fld3d, c(3,2,1))
     dim(fld) <- c(ntime, nlat*nlon)
-    
+
     ## Return the array.  It has cases in rows and grid cells in columns.
     fld
 }
@@ -77,7 +82,7 @@ readglobmean <- function(filename, suffix='_gavg.txt')
 {
     gfilename <- paste0(filename,suffix)
     tbl <- read.table(gfilename)
-    
+
     ## Return as an nx1 matrix
     matrix(tbl[[1]], ncol=1)
 }
